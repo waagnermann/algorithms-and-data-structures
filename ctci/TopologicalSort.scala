bfcc4a30c62b58bbe1abae3e70ae40a743fb7332
Filipp Vagner <f.vagner@altuera.com>
2020-10-22 14:00:11 +0600

    Implement topological sort for DAG with O(V + E)

--- ctci/TopologicalSort.scala
+++ ctci/TopologicalSort.scala
@@ -1,16 +1,34 @@
 import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.Stack
 
 object TopologicalSort {
 	class Graph(vertexNumber: Int) {
 		private val adj = Array.fill(vertexNumber)(ArrayBuffer.empty[Int])
 
 		def addEdge(vertexParent: Int, vertexChild: Int): Unit = {
-			adj(vertexParent).addOne(vertexChild)
+			adj(vertexParent).append(vertexChild)
 		}
 
 		override def toString: String = adj.zipWithIndex flatMap {
 			 case (vertexParent, index) => vertexParent map { vertexChild => (index, vertexChild) } 
-			} mkString(", ")
+		} mkString(", ")
+
+		def TopologicalSortUtil(vertexParentIndex: Int, visited: Array[Boolean], stack: Stack[Int]): Unit = {
+			if (!visited(vertexParentIndex)) {
+				visited(vertexParentIndex) = true
+				adj(vertexParentIndex) foreach (vertexChild => TopologicalSortUtil(vertexChild, visited, stack))
+				stack.push(vertexParentIndex)
+			}
+		}
+
+		def topologicalSort(): String = {
+			val stack = Stack.empty[Int]
+			val visited = Array.ofDim[Boolean](vertexNumber)
+
+			adj.zipWithIndex foreach { case (_, index) => TopologicalSortUtil(index, visited, stack)}
+
+			stack mkString ", "
+		}
 	}
 
 	def main(args: Array[String]): Unit = {
@@ -21,6 +39,7 @@ object TopologicalSort {
         graph.addEdge(4, 1) 
         graph.addEdge(2, 3) 
         graph.addEdge(3, 1)
-        println(graph)
+        println("Graph: " + graph)
+        println("TopologicalSort: " + graph.topologicalSort())
 	}
 }
