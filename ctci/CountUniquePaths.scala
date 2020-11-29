d52459dead3b29bfac715ebdd2e1591dea2e1af9
Filipp Vagner <f.vagner@altuera.com>
2020-11-30 05:07:30 +0600

    Update CountUniquePaths.scala

--- ctci/CountUniquePaths.scala
+++ ctci/CountUniquePaths.scala
@@ -1,18 +1,31 @@
+import scala.collection.mutable.Map
+
 object CountUniquePaths {
-	def countPaths(grid: Array[Array[Boolean]], row: Int, col: Int,
-	 maxRow: Int, maxCol: Int, cached: Map[Tuple2[Int, Int], Int]): Int = {
-	
-		if (nonValidPosition(grid, row, col, maxRow, maxCol)) 0 
-		else if (row == maxRow && col == maxCol) 1
-		else cached get (row, col) match {
-			case Some(count) => count
-			case None => 
-				val downwardsCache: Int = countPaths(grid, row + 1, col, maxRow, maxCol, cached)
-				val rightwardsCache: Int = countPaths(grid, row, col + 1, maxRow, maxCol, cached)
-				val cache: Int = downwardsCache + rightwardsCache
-				countPaths(grid, row, col, maxRow, maxCol, cached updated ((row, col), cache))
+	def countPaths(grid: Array[Array[Boolean]], cached: Map[Tuple2[Int, Int], Int]): Int = {
+		val (maxRow, maxCol): (Int, Int) = (grid.length - 1, grid(0).length - 1)
+
+		def run(grid: Array[Array[Boolean]], row: Int, col: Int, maxRow: Int, maxCol: Int): Int = {
+			if (nonValidPosition(grid, row, col, maxRow, maxCol)) 0 
+			else if (row == maxRow && col == maxCol) 1
+			else cached get (row, col) match {
+				case Some(count) => 
+					println(s"row: $row, col: $col (Some)")
+					count
+				case None => 
+					println(s"row: $row, col: $col (None)")
+					val downwardsCache: Int = run(grid, row + 1, col, maxRow, maxCol)
+					val rightwardsCache: Int = run(grid, row, col + 1, maxRow, maxCol)
+					val cache: Int = downwardsCache + rightwardsCache
+					cached((row, col)) = cache
+					//cache //use run(grid, row, col, maxRow, maxCol) to make caching more visible
+					run(grid, row, col, maxRow, maxCol)
+			}
 		}
+
+		run(grid, 0, 0, maxRow, maxCol)
 	}
+
+	
 
 	def nonValidPosition(grid: Array[Array[Boolean]], row: Int, col: Int, maxRow: Int, maxCol: Int): Boolean = {
 		if (row < 0 || row > maxRow || col < 0 || col > maxCol || !grid(row)(col)) true else false
@@ -30,7 +43,6 @@ object CountUniquePaths {
 			Array(true, true, true, true, true, true, true, true)
 			)
 
-		val (maxRow, maxCol): (Int, Int) = (grid.length - 1, grid(0).length - 1)
-		println("Unique paths count: " + countPaths(grid, 0, 0, maxRow, maxCol, Map.empty[Tuple2[Int, Int], Int]))
+		println("Unique paths count: " + countPaths(grid, Map.empty[Tuple2[Int, Int], Int]))
 	}
 }
