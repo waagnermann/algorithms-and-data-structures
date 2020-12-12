fcbf18c706fcfbf0de783ed65f59371460ff2a03
waagnermann <waagnermann@gmail.com>
2020-12-13 04:47:56 +0600

    Update MaxProfit.scala

--- ctci/MaxProfit.scala
+++ ctci/MaxProfit.scala
@@ -1,4 +1,10 @@
 object MaxProfit {
+	/*Say you have an array for which the ith element is the price of a given stock on day i.
+
+	If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock),
+	design an algorithm to find the maximum profit.
+
+	Note that you cannot sell a stock before you buy one.*/
 
 	def maxProfit(prices: Array[Int]): Int = {
 		val (buyFor, sellFor, profit) = (Int.MaxValue, 0, 0)
