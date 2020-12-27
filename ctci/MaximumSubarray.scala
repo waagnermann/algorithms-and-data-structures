e532964087b2eb6cf8c425b769ad95f7adc7acf9
Filipp Vagner <f.vagner@altuera.com>
2020-12-28 05:40:01 +0600

    Solve MaximumSubarray problem for O(n) time, O(1) space

---
+++ ctci/MaximumSubarray.scala
@@ -0,0 +1,28 @@
+ import scala.math.max
+
+object MaximumSubarray {
+	
+	def maxSubArray(nums: Array[Int]): Int = {
+		var runningSum = 0
+		var maxSum = Int.MinValue
+		val volume = nums.length
+
+		var i = 0
+		while (i < volume) {
+			val num = nums(i)
+			val currentSum = runningSum + num
+			maxSum = max(currentSum, maxSum)
+			runningSum = if (currentSum < 0) 0 else currentSum
+			i += 1
+		}
+
+		maxSum
+	}
+
+	def main(args: Array[String]): Unit = {
+		val input: Array[Int] = Array(-2, 1, -3, 4, -1, 2, 1, -5, 4)
+		val output: Int = maxSubArray(input)
+		println(s"Input: nums = ${input mkString ("[", ",", "]")}")
+		println(s"Output: $output")
+	}
+}
