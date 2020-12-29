9c69a4d9f1cd55386afa83a11b1067d6de65fdbd
Filipp Vagner <f.vagner@altuera.com>
2020-12-29 14:08:24 +0600

    Update MaximumProductSubarray.scala

--- ctci/MaximumProductSubarray.scala
+++ ctci/MaximumProductSubarray.scala
@@ -11,13 +11,20 @@ object MaximumProductSubarray {
         val volume = nums.length
         var maximumProduct = Int.MinValue
         var runningProduct = 1
+        var divider = 1
 
         var i = 0
         while (i < volume) {
         	val currentNumber = nums(i)
         	val currentProduct = runningProduct * currentNumber
-        	maximumProduct = max(currentProduct, maximumProduct)
+        	maximumProduct = max(max(currentProduct, maximumProduct), currentProduct / divider)
         	runningProduct = if (currentProduct == 0) 1 else currentProduct
+        	if (divider == 1 && currentProduct < 0) {
+        		divider = currentProduct
+        	}
+        	if (currentProduct == 0) {
+        		divider = 1
+        	}
         	i += 1
         }
 
@@ -25,7 +32,7 @@ object MaximumProductSubarray {
     }
 
 	def main(args: Array[String]): Unit = {
-		val input: Array[Int] = Array(-2, 3, -2, 4)
+		val input: Array[Int] = Array(2,-5,-2,-4,3)
 		val output: Int = maxProduct(input)
 		println(s"Input: ${input mkString ("[", ",", "]")}")
 		println(s"Output: $output")
